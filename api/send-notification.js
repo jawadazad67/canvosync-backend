7239634ad721d525306125524ad4ce0cb566e49e
jawadazad67 <jawaadahmadazad67@gmail.com>
2025-09-25 13:24:13 +0500

    Add sound to notification payload

--- api/send-notification.js
+++ api/send-notification.js
@@ -17,15 +17,6 @@ const db = admin.firestore();
 const messaging = admin.messaging();
 
 export default async function handler(req, res) {
-    // Allow requests from any origin
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
@@ -57,17 +48,14 @@ export default async function handler(req, res) {
       }
     }
 
-   // Prepare FCM payload
-const payload = {
-  notification: {
-    title: `${senderName}`,
-    body: message,
-    sound: "default", // ensures sound
-  },
-
-  token: fcmToken,
-};
-
+    // Prepare FCM payload
+    const payload = {
+      notification: {
+        title: `${senderName}`,
+        body: message,
+      },
+      token: fcmToken,
+    };
 
     // Send notification
     const response = await messaging.send(payload);
