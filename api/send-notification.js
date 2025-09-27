ca4089238f2061792a51ddfdf620e1c2f89ad7da
jawadazad67 <jawaadahmadazad67@gmail.com>
2025-09-27 19:59:37 +0500

    group notification api

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,17 +1,4 @@
-import admin from 'firebase-admin';
-// Initialize Firebase only once
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(
-        {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }
-    ),
-
-  });
-}
+import admin from "../shared/firebaseAdmin.js"
 
 const db = admin.firestore();
 const messaging = admin.messaging();
