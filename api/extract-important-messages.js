63e1037f90110123bb1d63cb6c96b1e715577e59
jawadazad67 <jawaadahmadazad67@gmail.com>
2025-09-29 01:01:40 +0500

    date time

--- api/extract-important-messages.js
+++ api/extract-important-messages.js
@@ -37,7 +37,7 @@ Rules:
 - If only a day (like Monday, tomorrow) is given and no exact time is given and its important then → set time as 09:00 AM.
 - If urgent and today → set datetime = 1 hour after current time (${currentDate} ${currentTime}).
 - important = 0 otherwise.
-- datetime must be in Pakistan Standard Time (UTC+5).
+- datetime must be in Standard Time (UTC ) no extra 5 plus for pakistan.
 - impotant =0 if date-time is previous or behind current date/time then its not important ,same for yesterday , or any previous date.  
 - Today is ${currentDate}, and current time is ${currentTime}.
 `;
