6b1aea66f918cd5f74b717d2279fa382eea61453
jawadazad67 <jawaadahmadazad67@gmail.com>
2025-09-29 21:01:42 +0500

    cors access allowing

--- api/extract-important-messages.js
+++ api/extract-important-messages.js
@@ -3,6 +3,16 @@ import admin from "../shared/firebaseAdmin.js";
 
 const db = admin.firestore();
 export default async function handler(req, res) {
+res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle preflight request (OPTIONS)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
